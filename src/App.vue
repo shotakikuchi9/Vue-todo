778cc1648532c9222d96442318e5f379e7805324
菊池翔太 <kikuchishouta@kikuchishoutanoMacBook-Pro.local>
2021-01-14 17:27:38 +0900

    Vue3

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1>Todoリスト</h1>
+    <div>
+      <label><input type="radio" value="全て" name="status" checked @click="currentStatus = 'All'">全て</label>
+      <label><input type="radio" value="作業中" name="status" @click="currentStatus = 'Working'">作業中</label>
+      <label><input type="radio" value="完了" name="status" @click="currentStatus = 'Complete'">完了</label>
+    </div>
+    <div>          
+      <component :is="currentStatus" :tasks="tasks"></component>
+    </div>
+    <div>                       
+      <h2>新規タスクの追加</h2>  
+    </div>                          
+    <div>  
+      <input type="text" class="task" v-model="content">
+      <button @click="addTask">追加</button>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Complete from './components/Complete'
+import Working from './components/Working'
+import All from './components/All'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Complete,
+    Working,
+    All
+  },
+  data() {
+    return {
+      tasks: [],
+      content: '',
+      state: '作業中',
+      currentStatus: 'All'
+    }
+  },
+  methods: {
+    addTask() {
+      this.tasks.push({content: this.content, state: this.state});
+      this.content = '';
+    }
   }
 }
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.task {
+  margin-right: 12px;
 }
 </style>
